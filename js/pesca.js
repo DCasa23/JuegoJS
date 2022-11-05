a611868bdafd5fff993d8e1ba733226e2d783b0a
DCasa23 <davidcasado1988@yahoo.es>
2022-11-05 22:54:54 +0100

    Fix px and coords "Pesca"

--- js/pesca.js
+++ js/pesca.js
@@ -1,23 +1,81 @@
 
 function lanzarCaña() {
     valor2 = 140;
-    valor = Math.floor(Math.random() * 1200)
+    valor = Math.floor(Math.random() * 1080)
+    
     document.getElementById("anzuelo").style.marginLeft = valor + "px";
     document.getElementById("anzuelo").style.marginTop = valor2 + "px";
     var movimientoVaca = setInterval(myCallback, 1000);
     function myCallback() {
 
-        if (anzuelo.style.marginTop < 600 + "px") {
-            /* if (valor - 100 < valor2) {
-                 location.href = "http://www.google.com";
+        if (anzuelo.style.marginTop<= 430 + "px") {
+             if (valor> 150 && valor<= 200  && valor2> 190  && valor2<= 245 ) {
+                 location.href = "http://www.abc.com";
          
              }
+             if (valor> 150 && valor< 225  && valor2> 280  && valor2<= 335 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 375 && valor< 450  && valor2> 345  && valor2<410 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 350 && valor< 425  && valor2> 255  && valor2<315 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 375 && valor< 475  && valor2> 175  && valor2<250 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 500 && valor< 625  && valor2> 230  && valor2<305 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 575 && valor< 650  && valor2> 135  && valor2<180 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 625 && valor< 700  && valor2> 300  && valor2<350 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 675 && valor< 750  && valor2> 200  && valor2<250 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 750 && valor< 850  && valor2> 270  && valor2<365 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 875 && valor< 950  && valor2> 245  && valor2<300 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 925 && valor< 975  && valor2> 160  && valor2<215 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 950 && valor< 1050  && valor2> 315  && valor2<375 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 1025 && valor< 1075  && valor2> 255  && valor2<305 ) {
+                location.href = "http://www.abc.com";
+        
+            }
+            if (valor> 105 && valor<= 140  && valor2> 405  && valor2<= 430 ) {
+                location.href = "http://www.lidl.com";
+        
+            }
+             /*
              if (valor > 1294) {
                  location.href = "http://www.marca.com";
          
              }*/
 
-            console.log("El valor Vaca es:" + anzuelo.style.marginLeft + "y el valor real: " + valor2);
+            console.log("El valor Vaca es:" + valor + " y el valor real: " + valor2);
             valor2 = valor2 + 5;
             document.getElementById("anzuelo").style.transitionDuration = "1s";
             document.getElementById("anzuelo").style.marginTop = valor2 + "px";
@@ -41,14 +99,14 @@ function correr(e) {
 
             // left arrow
         }
-        if (e.keyCode == '39'&& valor < 1200) {
+        if (e.keyCode == '39'&& valor < 1080 ) {
             valor = 25 + valor;
             anzuelo.style.marginLeft = valor + "px";
 
 
             // right arrow
         }
-        if (e.keyCode == '40'&& anzuelo.style.marginTop < 600 + "px") {
+        if (e.keyCode == '40'&& anzuelo.style.marginTop < 420 + "px") {
             valor2 = valor2 + 25;
             anzuelo.style.marginTop = valor2 + "px";
         }
@@ -62,17 +120,21 @@ function correr(e) {
     
 }
 
-
+/*
 function showCoords(event) {
-    var x = event.clientX;
-    var y = event.clientY;
+  var bounds = event.target.getBoundingClientRect();
+  var x = event.clientX - bounds.left;
+  var y = event.clientY - bounds.top;
     var coords = "X coords: " + x + ", Y coords: " + y;
-    document.getElementById("demo").innerHTML = coords;
+    
+    console.log(coords);
+    
     if (event.clientX>=250&&event.clientX<=280){
         location.href = "http://www.google.com";
     }
+    
   }
-
+*/
 
 
 
