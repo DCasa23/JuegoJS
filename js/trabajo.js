86eef9fd3683863c5827450fa2efb4f42b428cdd
DCasa23 <davidcasado1988@yahoo.es>
2022-11-07 12:17:15 +0100

    add ending

--- js/trabajo.js
+++ js/trabajo.js
@@ -1,9 +1,7 @@
 function aceptarTrabajo(){
-    document.getElementById("contenedor").style.background="url('../images/vencedor.jpeg')";
+    document.getElementById("contenedor").style.background="url('../images/vencedor.jfif')";
     document.getElementById("contenedor").style.backgroundSize = "1500px 800px";
-    const audio = document.createElement("audio");
-    audio.preload = "auto";
-    audio.src = "https://github.com/DCasa23/JuegoJS/blob/main/others/LongRoad.mp3";
+    const audio = new Audio("../others/LongRoad.mp3");
     audio.play();
-    document.body.appendChild(audio);
+    
 }
