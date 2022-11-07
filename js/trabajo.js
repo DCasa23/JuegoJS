0005a62cdab7b1afca951683aabb5957deba17c1
DCasa23 <davidcasado1988@yahoo.es>
2022-11-07 12:07:51 +0100

    Load Song

--- js/trabajo.js
+++ js/trabajo.js
@@ -3,7 +3,7 @@ function aceptarTrabajo(){
     document.getElementById("contenedor").style.backgroundSize = "1500px 800px";
     const audio = document.createElement("audio");
     audio.preload = "auto";
-    audio.src = "https://manzdev.github.io/codevember2017/assets/eye-tiger.mp3";
+    audio.src = "https://github.com/DCasa23/JuegoJS/blob/main/others/LongRoad.mp3";
     audio.play();
     document.body.appendChild(audio);
 }
